2fb862ed341efc90af378706a101328d9e3aa347
Eli Arbel <aelij@arbel.net>
2016-02-27 21:17:31 +0200

    Move editor popup with Window

--- src/RoslynPad/Editor/ContextActionsRenderer.cs
+++ src/RoslynPad/Editor/ContextActionsRenderer.cs
@@ -20,31 +20,15 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Threading;
-using ICSharpCode.AvalonEdit;
-using ICSharpCode.AvalonEdit.Document;
-using ICSharpCode.AvalonEdit.Rendering;
 
 namespace RoslynPad.Editor
 {
-    internal interface IContextActionProvider
-    {
-        Task<IEnumerable<object>> GetActions(int offset, int length, CancellationToken cancellationToken);
-
-        ICommand GetActionCommand(object action);
-    }
-
     internal sealed class ContextActionsRenderer : IDisposable
     {
         private const int DelayMoveMilliseconds = 500;
@@ -74,10 +58,51 @@ namespace RoslynPad.Editor
             _delayMoveTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(DelayMoveMilliseconds) };
             _delayMoveTimer.Stop();
             _delayMoveTimer.Tick += TimerMoveTick;
+
+            if (editor.IsLoaded)
+            {
+                HookupWindowMove();
+            }
+            else
+            {
+                editor.Loaded += OnEditorLoaded;
+            }
+        }
+
+        private void OnEditorLoaded(object sender, RoutedEventArgs e)
+        {
+            _editor.Loaded -= OnEditorLoaded;
+
+            HookupWindowMove();
+        }
+
+        private void HookupWindowMove()
+        {
+            var window = Window.GetWindow(_editor);
+            if (window != null)
+            {
+                window.LocationChanged += WindowOnLocationChanged;
+            }
+        }
+
+        private void WindowOnLocationChanged(object sender, EventArgs eventArgs)
+        {
+            if (_popup?.IsOpen == true)
+            {
+                _popup.HorizontalOffset += double.Epsilon;
+                _popup.HorizontalOffset -= double.Epsilon;
+            }
         }
 
         public void Dispose()
         {
+            _editor.Loaded -= OnEditorLoaded;
+            var window = Window.GetWindow(_editor);
+            if (window != null)
+            {
+                window.LocationChanged -= WindowOnLocationChanged;
+            }
+
             ClosePopup();
         }
 
@@ -213,213 +238,6 @@ namespace RoslynPad.Editor
                 _popup.IsMenuOpen = false;
                 _popup.ItemsSource = null;
             }
-        }
-    }
-
-    internal class ExtendedPopup : Popup
-    {
-        private readonly UIElement _parent;
-
-        public ExtendedPopup(UIElement parent)
-        {
-            if (parent == null) throw new ArgumentNullException(nameof(parent));
-            _parent = parent;
-        }
-
-        public new bool IsOpen => base.IsOpen;
-
-        private bool _openIfFocused;
-
-        public bool IsOpenIfFocused
-        {
-            get { return _openIfFocused; }
-            set
-            {
-                if (_openIfFocused != value)
-                {
-                    _openIfFocused = value;
-                    if (value)
-                    {
-                        _parent.IsKeyboardFocusedChanged += parent_IsKeyboardFocusedChanged;
-                    }
-                    else {
-                        _parent.IsKeyboardFocusedChanged -= parent_IsKeyboardFocusedChanged;
-                    }
-                    OpenOrClose();
-                }
-            }
-        }
-
-        private void parent_IsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            OpenOrClose();
-        }
-
-        protected override void OnIsKeyboardFocusWithinChanged(DependencyPropertyChangedEventArgs e)
-        {
-            base.OnIsKeyboardFocusWithinChanged(e);
-            OpenOrClose();
-        }
-
-        private void OpenOrClose()
-        {
-            var newIsOpen = _openIfFocused && (_parent.IsKeyboardFocused || IsKeyboardFocusWithin);
-            base.IsOpen = newIsOpen;
-        }
-    }
-
-    internal sealed class ContextActionsBulbPopup : ExtendedPopup
-    {
-        private readonly MenuItem _mainItem;
-
-        public ContextActionsBulbPopup(UIElement parent) : base(parent)
-        {
-            UseLayoutRounding = true;
-            TextOptions.SetTextFormattingMode(this, TextFormattingMode.Display);
-
-            StaysOpen = true;
-            AllowsTransparency = true;
-            _mainItem = new MenuItem
-            {
-                ItemContainerStyle = CreateItemContainerStyle(),
-                Header = new Image
-                {
-                    Source = TryFindResource("Bulb") as ImageSource,
-                    Width = 16,
-                    Height = 16
-                }
-            };
-            var menu = new Menu
-            {
-                Background = Brushes.Transparent,
-                BorderBrush = _mainItem.BorderBrush,
-                BorderThickness = _mainItem.BorderThickness,
-                Items = { _mainItem }
-            };
-            Child = menu;
-        }
-
-        private Style CreateItemContainerStyle()
-        {
-            var style = new Style(typeof(MenuItem));
-            style.Setters.Add(new Setter(MenuItem.CommandProperty,
-                new Binding { Converter = new ActionCommandConverter(this) }));
-            style.Seal();
-            return style;
-        }
-
-        public IEnumerable<object> ItemsSource
-        {
-            get { return (IEnumerable<object>)_mainItem.ItemsSource; }
-            set { _mainItem.ItemsSource = value; }
-        }
-
-        public bool HasItems => _mainItem.HasItems;
-
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            base.OnKeyDown(e);
-            if (e.Key == Key.Escape)
-                IsOpenIfFocused = false;
-        }
-
-        public void Close()
-        {
-            IsOpenIfFocused = false;
-        }
-
-        public bool IsMenuOpen
-        {
-            get { return _mainItem.IsSubmenuOpen; }
-            set { _mainItem.IsSubmenuOpen = value; }
-        }
-
-        public Func<object, ICommand> CommandProvider { get; set; }
-
-        public new void Focus()
-        {
-            Child.Focus();
-        }
-
-        public void OpenAtLineStart(CodeTextEditor editor)
-        {
-            SetPosition(this, editor, editor.TextArea.Caret.Line, 1);
-            VerticalOffset -= 16;
-            IsOpenIfFocused = true;
-        }
-
-        private static void SetPosition(Popup popup, TextEditor editor, int line, int column, bool openAtWordStart = false)
-        {
-            var document = editor.Document;
-            var offset = document.GetOffset(line, column);
-            if (openAtWordStart)
-            {
-                var wordStart = document.FindPreviousWordStart(offset);
-                if (wordStart != -1)
-                {
-                    var wordStartLocation = document.GetLocation(wordStart);
-                    line = wordStartLocation.Line;
-                    column = wordStartLocation.Column;
-                }
-            }
-            var caretScreenPos = editor.TextArea.TextView.GetScreenPosition(line, column);
-            popup.HorizontalOffset = caretScreenPos.X;
-            popup.VerticalOffset = caretScreenPos.Y;
-            popup.Placement = PlacementMode.Absolute;
-        }
-
-        private class ActionCommandConverter : IValueConverter
-        {
-            private readonly ContextActionsBulbPopup _owner;
-
-            public ActionCommandConverter(ContextActionsBulbPopup owner)
-            {
-                _owner = owner;
-            }
-
-            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                return _owner.CommandProvider?.Invoke(value);
-            }
-
-            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                throw new NotSupportedException();
-            }
-        }
-
-    }
-
-    internal static class DocumentUtilities
-    {
-        public static int FindPreviousWordStart(this ITextSource textSource, int offset)
-        {
-            return TextUtilities.GetNextCaretPosition(textSource, offset, LogicalDirection.Backward, CaretPositioningMode.WordStart);
-        }
-    }
-
-    internal static class TextViewExtensions
-    {
-        public static Point GetScreenPosition(this TextView textView, int line, int column)
-        {
-            var visualPosition = textView.GetVisualPosition(
-                new TextViewPosition(line, column), VisualYPosition.LineBottom) - textView.ScrollOffset;
-            var positionInPixels = textView.PointToScreen(new Point(visualPosition.X.CoerceValue(0, textView.ActualWidth),
-                                                                    visualPosition.Y.CoerceValue(0, textView.ActualHeight)));
-            return positionInPixels.TransformFromDevice(textView);
-        }
-
-        private static double CoerceValue(this double value, double minimum, double maximum)
-        {
-            return Math.Max(Math.Min(value, maximum), minimum);
-        }
-
-        private static Point TransformFromDevice(this Point point, Visual visual)
-        {
-            var compositionTarget = PresentationSource.FromVisual(visual)?.CompositionTarget;
-            if (compositionTarget == null) throw new InvalidOperationException("Invalid visual");
-            var matrix = compositionTarget.TransformFromDevice;
-            return new Point(point.X * matrix.M11, point.Y * matrix.M22);
         }
     }
 }
