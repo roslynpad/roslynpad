67c63fc718d0ead9648efad2331a6e0d6f26e300
Virgile Bello <virgile@siliconstudio.co.jp>
2016-09-30 14:01:36 +0900

    Provide a way to turn on/off workspace diagnostics

---
+++ src/RoslynPad.Roslyn/WorkspaceExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.CodeAnalysis;
+
+namespace RoslynPad.Roslyn
+{
+    /// <summary>
+    /// Provide a way for users to turn on and off analyzing workspace for compiler diagnostics
+    /// </summary>
+    public static class WorkspaceExtensions
+    {
+        public static void EnableDiagnostics(this Workspace workspace, DiagnosticOptions options)
+        {
+            var diagnosticProviderOptions = (DiagnosticProvider.Options)0;
+            if ((options & DiagnosticOptions.Syntax) != 0)
+                diagnosticProviderOptions |= DiagnosticProvider.Options.Syntax;
+            if ((options & DiagnosticOptions.Semantic) != 0)
+                diagnosticProviderOptions |= DiagnosticProvider.Options.Semantic;
+
+            DiagnosticProvider.Enable(workspace, diagnosticProviderOptions);
+        }
+
+        public static void DisableDiagnostics(this Workspace workspace)
+        {
+            DiagnosticProvider.Disable(workspace);
+        }
+    }
+}
