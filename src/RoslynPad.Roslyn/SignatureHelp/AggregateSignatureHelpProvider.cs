f5aac1d64c5efd3eb37c2466a2f530112445c56c
Eli Arbel <aelij@arbel.net>
2018-12-08 16:09:55 +0200

    Signature help update

--- src/RoslynPad.Roslyn/SignatureHelp/AggregateSignatureHelpProvider.cs
+++ src/RoslynPad.Roslyn/SignatureHelp/AggregateSignatureHelpProvider.cs
@@ -66,14 +66,16 @@ namespace RoslynPad.Roslyn.SignatureHelp
                 var items = new SignatureHelpItems(bestItems);
                 if (items.SelectedItemIndex == null)
                 {
-                    var bestItem = GetBestItem(null, items.Items, items.ArgumentCount, items.ArgumentName, isCaseSensitive: true);
-                    if (bestItem != null)
+                    var selection = DefaultSignatureHelpSelector.GetSelection(items.Items, null, false, items.ArgumentIndex, items.ArgumentCount, items.ArgumentName, isCaseSensitive: true);
+                    if (selection.SelectedItem != null)
                     {
-                        items.SelectedItemIndex = items.Items.IndexOf(bestItem);
+                        items.SelectedItemIndex = items.Items.IndexOf(selection.SelectedItem);
                     }
                 }
+
                 return items;
             }
+
             return null;
         }
 
@@ -82,15 +84,65 @@ namespace RoslynPad.Roslyn.SignatureHelp
             return bestItems == null || currentTextSpan?.Start > bestItems.ApplicableSpan.Start;
         }
 
-        private static SignatureHelpItem GetBestItem(SignatureHelpItem currentItem, ICollection<SignatureHelpItem> filteredItems, int argumentCount, string name, bool isCaseSensitive)
+        private struct SignatureHelpSelection
         {
-            while (true)
+            public SignatureHelpSelection(SignatureHelpItem selectedItem, bool userSelected, int? selectedParameter)
+            {
+                SelectedItem = selectedItem;
+                UserSelected = userSelected;
+                SelectedParameter = selectedParameter;
+            }
+
+            public int? SelectedParameter { get; }
+            public SignatureHelpItem SelectedItem { get; }
+            public bool UserSelected { get; }
+        }
+
+        private static class DefaultSignatureHelpSelector
+        {
+            public static SignatureHelpSelection GetSelection(
+                IList<SignatureHelpItem> items,
+                SignatureHelpItem selectedItem,
+                bool userSelected,
+                int argumentIndex,
+                int argumentCount,
+                string argumentName,
+                bool isCaseSensitive)
+            {
+                SelectBestItem(ref selectedItem, ref userSelected, items, argumentIndex, argumentCount, argumentName, isCaseSensitive);
+                var selectedParameter = GetSelectedParameter(selectedItem, argumentIndex, argumentName, isCaseSensitive);
+                return new SignatureHelpSelection(selectedItem, userSelected, selectedParameter);
+            }
+
+            private static int GetSelectedParameter(SignatureHelpItem bestItem, int parameterIndex, string parameterName, bool isCaseSensitive)
+            {
+                if (!string.IsNullOrEmpty(parameterName))
+                {
+                    var comparer = isCaseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase;
+                    var selected = bestItem.Parameters.Select((p, index) => (p, index)).FirstOrDefault(p => comparer.Equals(p.p.Name, parameterName));
+                    if (selected.p != null)
+                    {
+                        return selected.index;
+                    }
+                }
+
+                return parameterIndex;
+            }
+
+            private static void SelectBestItem(ref SignatureHelpItem currentItem, ref bool userSelected,
+                IList<SignatureHelpItem> filteredItems, int selectedParameter, int argumentCount, string name, bool isCaseSensitive)
             {
                 // If the current item is still applicable, then just keep it.
-                if (currentItem != null && filteredItems.Contains(currentItem) && IsApplicable(currentItem, argumentCount, name, isCaseSensitive))
+                if (filteredItems.Contains(currentItem) &&
+                    IsApplicable(currentItem, argumentCount, name, isCaseSensitive))
                 {
-                    return currentItem;
+                    // If the current item was user-selected, we keep it as such.
+                    return;
                 }
+
+                // If the current item is no longer applicable, we'll be choosing a new one,
+                // which was definitely not previously user-selected.
+                userSelected = false;
 
                 // Try to find the first applicable item.  If there is none, then that means the
                 // selected parameter was outside the bounds of all methods.  i.e. all methods only
@@ -99,59 +151,60 @@ namespace RoslynPad.Roslyn.SignatureHelp
                 var result = filteredItems.FirstOrDefault(i => IsApplicable(i, argumentCount, name, isCaseSensitive));
                 if (result != null)
                 {
-                    return result;
+                    currentItem = result;
+                    return;
                 }
 
                 // if we couldn't find a best item, and they provided a name, then try again without
                 // a name.
                 if (name != null)
                 {
-                    name = null;
-                    continue;
+                    SelectBestItem(ref currentItem, ref userSelected, filteredItems, selectedParameter, argumentCount, null, isCaseSensitive);
+                    return;
                 }
 
                 // If we don't have an item that can take that number of parameters, then just pick
                 // the last item.  Or stick with the current item if the last item isn't any better.
                 var lastItem = filteredItems.Last();
-                if (currentItem != null && (currentItem.IsVariadic || currentItem.Parameters.Length == lastItem.Parameters.Length))
+                if (currentItem.IsVariadic || currentItem.Parameters.Length == lastItem.Parameters.Length)
                 {
-                    return currentItem;
+                    return;
                 }
 
-                return lastItem;
+                currentItem = lastItem;
             }
-        }
 
-        private static bool IsApplicable(SignatureHelpItem item, int argumentCount, string name, bool isCaseSensitive)
-        {
-            // If they provided a name, then the item is only valid if it has a parameter that
-            // matches that name.
-            if (name != null)
+            private static bool IsApplicable(SignatureHelpItem item, int argumentCount, string name, bool isCaseSensitive)
             {
-                var comparer = isCaseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase;
-                return item.Parameters.Any(p => comparer.Equals(p.Name, name));
-            }
+                // If they provided a name, then the item is only valid if it has a parameter that
+                // matches that name.
+                if (name != null)
+                {
+                    var comparer = isCaseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase;
+                    return item.Parameters.Any(p => comparer.Equals(p.Name, name));
+                }
 
-            // An item is applicable if it has at least as many parameters as the selected
-            // parameter index.  i.e. if it has 2 parameters and we're at index 0 or 1 then it's
-            // applicable.  However, if it has 2 parameters and we're at index 2, then it's not
-            // applicable.  
-            if (item.Parameters.Length >= argumentCount)
-            {
-                return true;
-            }
+                // An item is applicable if it has at least as many parameters as the selected
+                // parameter index.  i.e. if it has 2 parameters and we're at index 0 or 1 then it's
+                // applicable.  However, if it has 2 parameters and we're at index 2, then it's not
+                // applicable.
+                if (item.Parameters.Length >= argumentCount)
+                {
+                    return true;
+                }
 
-            // However, if it is variadic then it is applicable as it can take any number of
-            // items.
-            if (item.IsVariadic)
-            {
-                return true;
-            }
+                // However, if it is variadic then it is applicable as it can take any number of
+                // items.
+                if (item.IsVariadic)
+                {
+                    return true;
+                }
 
-            // Also, we special case 0.  that's because if the user has "Foo(" and foo takes no
-            // arguments, then we'll see that it's arg count is 0.  We still want to consider
-            // any item applicable here though.
-            return argumentCount == 0;
+                // Also, we special case 0.  that's because if the user has "Goo(" and goo takes no
+                // arguments, then we'll see that it's arg count is 0.  We still want to consider
+                // any item applicable here though.
+                return argumentCount == 0;
+            }
         }
     }
 }
