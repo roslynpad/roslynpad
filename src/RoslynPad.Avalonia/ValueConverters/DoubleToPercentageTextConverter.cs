fc818ff966cfa145c3fe7bd1003b06d4a7efa5e8
Milan Stanacev <stanacevm@gmail.com>
2020-01-13 18:03:33 +0100

    Support progress reportinging in Avalonia UI

---
+++ src/RoslynPad.Avalonia/ValueConverters/DoubleToPercentageTextConverter.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace RoslynPad.Converters
+{
+    public class DoubleToPercentageTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double percent;
+
+            if (value is double?)
+            {
+                percent = (value as double?) ?? 0;
+            }
+            else
+            {
+                try
+                {
+                    percent = (double)value;
+                }
+                catch
+                {
+                    percent = 0.0;
+                }
+            }
+
+            if (percent <= 0) return "0%";
+            if (percent >= 1) return "100%";
+
+            return ((int)Math.Round(percent * 100.0, 0)) + "%";
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
