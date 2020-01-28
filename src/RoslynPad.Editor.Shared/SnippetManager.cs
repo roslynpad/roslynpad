2ee6f50e936dbf3f5ec10245bb650a629abd0a90
Kirides <john.heckendorf@outlook.com>
2020-01-28 09:21:33 +0100

    Update src/RoslynPad.Editor.Shared/SnippetManager.cs
    Suggestion by @aelij
    
    Co-Authored-By: Eli Arbel <aelij@arbel.net>

--- src/RoslynPad.Editor.Shared/SnippetManager.cs
+++ src/RoslynPad.Editor.Shared/SnippetManager.cs
@@ -158,7 +158,7 @@ namespace RoslynPad.Editor
                 "desktopapp",
                 "#r Framework-include and await Helpers.RunWpfAsync()",
                 "#r \"framework:Microsoft.WindowsDesktop.App\"\nawait Helpers.RunWpfAsync();\n\n${Selection}",
-                "#r"
+                "desktopapp"
             )
         }.ToImmutableDictionary(x => x.Name);
 
