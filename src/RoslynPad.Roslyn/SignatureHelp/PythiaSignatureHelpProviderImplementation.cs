a4e1cd98c3fb14cffc3bccef1568a74942ff8c7c
Eli Arbel <elarbel@microsoft.com>
2020-03-21 11:57:57 +0200

    Fix Roslyn dependency

---
+++ src/RoslynPad.Roslyn/SignatureHelp/PythiaSignatureHelpProviderImplementation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.ExternalAccess.Pythia.Api;
+
+namespace RoslynPad.Roslyn.SignatureHelp
+{
+    [Export(typeof(IPythiaSignatureHelpProviderImplementation))]
+    internal class PythiaSignatureHelpProviderImplementation : IPythiaSignatureHelpProviderImplementation
+    {
+        public Task<(ImmutableArray<PythiaSignatureHelpItemWrapper> items, int? selectedItemIndex)> GetMethodGroupItemsAndSelectionAsync(
+            ImmutableArray<IMethodSymbol> accessibleMethods, Document document, InvocationExpressionSyntax invocationExpression, SemanticModel semanticModel, SymbolInfo currentSymbol, CancellationToken cancellationToken)
+        {
+            return Task.FromResult((ImmutableArray<PythiaSignatureHelpItemWrapper>.Empty, (int?)null));
+        }
+    }
+}
