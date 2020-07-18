6e1f8883c0d8d469664fe077356dd9e331ff0c96
Eli Arbel <elarbel@microsoft.com>
2020-07-19 01:13:28 +0300

    Support C# 9 records

---
+++ src/RoslynPad.Runtime/IsExternalInit.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Dummy class so C# 'Record' types can compile on NetStandard and NetFx.
+    /// </summary>
+    public sealed class IsExternalInit { }
+}
