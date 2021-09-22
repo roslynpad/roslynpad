23da21cfbca1539dbe0057ea31bcdf128dd4b810
Eli Arbel <elarbel@microsoft.com>
2021-09-22 13:26:21 +0300

    Remove unused apis

--- src/RoslynPad.Roslyn/Diagnostics/DiagnosticsService.cs
+++ src/RoslynPad.Roslyn/Diagnostics/DiagnosticsService.cs
@@ -1,21 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Composition;
-using System.Linq;
-using System.Threading;
-using Microsoft.CodeAnalysis;
 
 namespace RoslynPad.Roslyn.Diagnostics
 {
     [Export(typeof(IDiagnosticService)), Shared]
     internal sealed class DiagnosticsService : IDiagnosticService
     {
-        private readonly Microsoft.CodeAnalysis.Diagnostics.IDiagnosticService _inner;
-
         [ImportingConstructor]
         public DiagnosticsService(Microsoft.CodeAnalysis.Diagnostics.IDiagnosticService inner)
         {
-            _inner = inner;
             inner.DiagnosticsUpdated += OnDiagnosticsUpdated;
         }
 
@@ -25,15 +18,5 @@ namespace RoslynPad.Roslyn.Diagnostics
         }
 
         public event EventHandler<DiagnosticsUpdatedArgs>? DiagnosticsUpdated;
-
-        public IEnumerable<DiagnosticData> GetDiagnostics(Workspace workspace, ProjectId projectId, DocumentId documentId, object id,
-            bool includeSuppressedDiagnostics, CancellationToken cancellationToken)
-        {
-            // TODO: migrate to diagnostics push/pull model
-#pragma warning disable CS0618 // Type or member is obsolete
-            return _inner.GetDiagnostics(workspace, projectId, documentId, id, includeSuppressedDiagnostics,
-                cancellationToken).Select(x => new DiagnosticData(x));
-#pragma warning restore CS0618 // Type or member is obsolete
-        }
     }
 }
--- src/RoslynPad.Roslyn/Diagnostics/IDiagnosticService.cs
+++ src/RoslynPad.Roslyn/Diagnostics/IDiagnosticService.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
-using Microsoft.CodeAnalysis;
 
 namespace RoslynPad.Roslyn.Diagnostics
 {
     public interface IDiagnosticService
     {
         event EventHandler<DiagnosticsUpdatedArgs> DiagnosticsUpdated;
-
-        IEnumerable<DiagnosticData> GetDiagnostics(Workspace workspace, ProjectId projectId, DocumentId documentId, object id, bool includeSuppressedDiagnostics, CancellationToken cancellationToken);
     }
 }
