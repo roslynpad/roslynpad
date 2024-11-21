b251c131818d725b123e742163ca70d4c48cc565
Randle-lanre <kennyrandle09@hotmail.co.uk>
2024-11-21 18:57:20 +0000

    cleanup

--- src/RoslynPad/ThemeDictionary.cs
+++ src/RoslynPad/ThemeDictionary.cs
@@ -51,7 +51,6 @@ public class ThemeDictionary : ThemeDictionaryBase
     public const string TabBorder = "tab.border";
     public const string InputBorder = "input.border";
     public const string TitleBarActiveBackground = "titleBar.activeBackground";
-    // public const string TextBoxBackground = "textBox.background";
     public const string InputBackground = "input.background";
     public const string InputForeground = "input.foreground";
     private void Initialize(Theme theme)
