f90d9399353a9576a1d2fe620bbe846d1d6950ca
Eli Arbel <496737+aelij@users.noreply.github.com>
2025-05-02 18:58:01 +0300

    Update push script

--- deploy/PushNuGet.ps1
+++ deploy/PushNuGet.ps1
@@ -3,7 +3,8 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Remove-Item -Recurse $PSScriptRoot\..\src\*.nupkg
 
-dotnet pack $PSScriptRoot\..\RoslynPad.sln -c Release -p:ContinuousIntegrationBuild=true
+dotnet pack $PSScriptRoot\..\RoslynPad.sln -c Release -p:EnableWindowsTargeting=true -p:ContinuousIntegrationBuild=true
 
+$apiKey = Read-Host -Prompt "Enter nuget.org API key"
 Get-ChildItem -Recurse $PSScriptRoot\..\src\*.nupkg | `
-    ForEach-Object { dotnet nuget push $_.FullName --source nuget.org }
+    ForEach-Object { dotnet nuget push $_.FullName --source nuget.org --api-key $apiKey }
