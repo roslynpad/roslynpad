a0b5daf2d85258e558a139bb30b2f703f8892bf3
danzk <danielknapik@gmail.com>
2025-11-13 13:18:59 +1100

    Fixed context actions not displayed properly in Avalonia

--- src/RoslynPad.Editor.Avalonia/ContextActionsBulbContextMenu.cs
+++ src/RoslynPad.Editor.Avalonia/ContextActionsBulbContextMenu.cs
@@ -1,4 +1,7 @@
 using Avalonia.Data;
+using Microsoft.CodeAnalysis.CodeActions;
+using RoslynPad.Roslyn.CodeActions;
+using RoslynPad.Roslyn.CodeFixes;
 
 namespace RoslynPad.Editor;
 
@@ -16,6 +19,8 @@ internal class ContextActionsBulbContextMenu : MenuFlyout
     {
         Setters =
         {
+            new Setter(MenuItem.HeaderProperty, new Binding { Converter = new CodeActionTitleConverter() }),
+            new Setter(MenuItem.ItemsSourceProperty, new Binding { Converter = new CodeActionsConverter() }),
             new Setter(MenuItem.CommandProperty, new Binding { Converter = _converter })
         }
     };
@@ -25,5 +30,41 @@ internal class ContextActionsBulbContextMenu : MenuFlyout
         var presenter = base.CreatePresenter();
         presenter.Styles.Add(CreateItemContainerStyle());
         return presenter;
+    }
+}
+
+internal class CodeActionTitleConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        return value switch
+        {
+            CodeAction codeAction => codeAction.Title,
+            CodeFix codeFix => codeFix.Action.Title,
+            _ => value?.ToString()
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+internal class CodeActionsConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        return value switch
+        {
+            CodeAction codeAction when codeAction.HasCodeActions() => codeAction.GetCodeActions(),
+            CodeFix codeFix when codeFix.Action.HasCodeActions() => codeFix.Action.GetCodeActions(),
+            _ => null
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotSupportedException();
     }
 }
