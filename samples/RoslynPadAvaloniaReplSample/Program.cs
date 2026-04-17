73aede9a5d040ae09e35417b72bbb6c39ae87749
Damir Lisak <damir.lisak@live.de>
2026-04-17 15:44:28 +0200

    Upgrade Roslynpad to AvaloniaEdit 12.0.0 https://github.com/roslynpad/roslynpad/issues/626

--- samples/RoslynPadAvaloniaReplSample/Program.cs
+++ samples/RoslynPadAvaloniaReplSample/Program.cs
@@ -5,7 +5,8 @@ namespace RoslynPadAvaloniaReplSample;
 internal static class Program
 {
     public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
-        .UsePlatformDetect();
+        .UsePlatformDetect()
+        .LogToTrace();
 
     public static int Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
