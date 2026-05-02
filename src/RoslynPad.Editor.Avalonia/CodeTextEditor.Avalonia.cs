5348251ba5299dae3b9d91465f7ee1630bad2a45
Eli Arbel <496737+aelij@users.noreply.github.com>
2026-05-02 14:12:01 +0300

    Avalonia search panel

--- src/RoslynPad.Editor.Avalonia/CodeTextEditor.Avalonia.cs
+++ src/RoslynPad.Editor.Avalonia/CodeTextEditor.Avalonia.cs
@@ -1,15 +1,23 @@
-﻿namespace RoslynPad.Editor;
+﻿using AvaloniaEdit.Search;
+
+namespace RoslynPad.Editor;
 
 public partial class CodeTextEditor
 {
     protected override Type StyleKeyOverride => typeof(TextEditor);
+
+    private SearchPanel? _searchReplacePanel;
 
     partial void Initialize()
     {
         PointerHover += OnMouseHover;
         PointerExited += OnPointerExited;
         KeyDownEvent.AddClassHandler<CodeTextEditor>(OnPreviewKeyDown, RoutingStrategies.Tunnel);
+
+        _searchReplacePanel = SearchPanel.Install(this);
     }
+
+    public SearchPanel SearchReplacePanel => _searchReplacePanel!;
 
     private void OnPointerExited(object? sender, PointerEventArgs e)
     {
